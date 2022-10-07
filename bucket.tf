7c144529786005cc930a5256ed1d2a99ee136f7b
venkysamrat <55248662+venkysamrat@users.noreply.github.com>
2022-10-07 16:24:22 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -2,7 +2,7 @@ provider "google" {
 project = var.project_id
 region  = var.region
 }
-resource "google_storage_bucket" "itd-aia-vkongara" {
+resource "google_storage_bucket" "itd-aia-vkongaraaa" {
 name     = var.bucket_name
 location = var.region
 }
